4e2b62ad4132fa1474e650c6e5662b8ca3fc1b98
stephenmwandau <stephen.mwandau@student.moringaschool.com>
2024-07-05 10:54:54 +0300

    dom manipulation

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+
+
+// Remove the <main> element
+document.getElementById("main").remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Assign an id of 'victory' to the new <h1> element
+newHeader.id = "victory";
+
+// Set the text of the new <h1> element
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// Append the new <h1> element to the body or desired container
+document.body.appendChild(newHeader);
